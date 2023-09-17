fc4a9b61759c3afe96469792e3b2fb7a17885174
Your Name <ayushgandhi904@gmail.com>
2023-09-17 17:28:54 +0530

    model_transformation added

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
 from dataclasses import dataclass
+import pickle
 
 
 host = os.getenv("host")
@@ -32,5 +33,19 @@ def read_sql_data():
         
         return df_train, df_test
         
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+    
+def save_object(file_path, obj):
+    
+    try:
+        dir_path = os.path.dirname(file_path)
+        
+        os.makedirs(dir_path, exist_ok = True)
+        
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
+                   
     except Exception as e:
         raise CustomException(e, sys)
